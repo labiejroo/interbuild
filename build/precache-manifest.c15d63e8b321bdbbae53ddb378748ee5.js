6c33ecd05deb3c2a82e8749ae251f8f4f517a0cc
Paweł Łabuz <pawel.labuz10@gmail.com>
2020-10-12 19:35:12 +0200

    update the README.md with link to contributing ge

---
+++ build/precache-manifest.c15d63e8b321bdbbae53ddb378748ee5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "467cf6b1f18eec2b3e41bbd4700f324d",
+    "url": "/labiejroo/interbuild.git/static/media/1.467cf6b1.jpg"
+  },
+  {
+    "revision": "3e2ee467894ec422ec88",
+    "url": "/labiejroo/interbuild.git/static/css/main.2a199053.chunk.css"
+  },
+  {
+    "revision": "5e782a7bbc0b079f279009bbf656ebea",
+    "url": "/labiejroo/interbuild.git/static/media/C.5e782a7b.jpg"
+  },
+  {
+    "revision": "8ae535edf8524437d876",
+    "url": "/labiejroo/interbuild.git/static/js/1.8ae535ed.chunk.js"
+  },
+  {
+    "revision": "d7b8ef9d58b65439c203",
+    "url": "/labiejroo/interbuild.git/static/js/runtime~main.d7b8ef9d.js"
+  },
+  {
+    "revision": "776c1d3e4aea7692c326770089ab66f0",
+    "url": "/labiejroo/interbuild.git/static/media/draft.776c1d3e.png"
+  },
+  {
+    "revision": "019d3d08c6b133af5aaeb33d7766a8bd",
+    "url": "/labiejroo/interbuild.git/static/media/wall.019d3d08.png"
+  },
+  {
+    "revision": "a1e9176b2461cc29ed69325c2de51f11",
+    "url": "/labiejroo/interbuild.git/static/media/A1.a1e9176b.jpg"
+  },
+  {
+    "revision": "4637d3099e80dd30d2c6251f21f6bc41",
+    "url": "/labiejroo/interbuild.git/static/media/A2.4637d309.jpg"
+  },
+  {
+    "revision": "55a8d084a05a690478c9aa6fdb17c7fd",
+    "url": "/labiejroo/interbuild.git/static/media/B2.55a8d084.jpg"
+  },
+  {
+    "revision": "5656861d2a81245a13f675aa265d37cf",
+    "url": "/labiejroo/interbuild.git/static/media/B1.5656861d.jpg"
+  },
+  {
+    "revision": "71e08996fad5a111a65c9e564de7ad3d",
+    "url": "/labiejroo/interbuild.git/static/media/C1.71e08996.jpg"
+  },
+  {
+    "revision": "8586f5002d7a3f1e70af6360148285fa",
+    "url": "/labiejroo/interbuild.git/static/media/C2.8586f500.jpg"
+  },
+  {
+    "revision": "3e2ee467894ec422ec88",
+    "url": "/labiejroo/interbuild.git/static/js/main.3e2ee467.chunk.js"
+  },
+  {
+    "revision": "aab89f53de4baa150ea90250b0f4a91c",
+    "url": "/labiejroo/interbuild.git/static/media/2.aab89f53.jpg"
+  },
+  {
+    "revision": "a6b7304af47d5225b0809b18800ec19e",
+    "url": "/labiejroo/interbuild.git/static/media/3.a6b7304a.jpg"
+  },
+  {
+    "revision": "284c41d9e67cb7e53967f59f7646aaf6",
+    "url": "/labiejroo/interbuild.git/static/media/4.284c41d9.jpg"
+  },
+  {
+    "revision": "0577c2fd14bd6a634fcb4bf1fe1e1e77",
+    "url": "/labiejroo/interbuild.git/static/media/5.0577c2fd.jpg"
+  },
+  {
+    "revision": "f564f4393fcf18c415bcaf1edda1d011",
+    "url": "/labiejroo/interbuild.git/static/media/6.f564f439.jpg"
+  },
+  {
+    "revision": "4294626170968f7c505f76db1e805a8a",
+    "url": "/labiejroo/interbuild.git/static/media/7.42946261.jpg"
+  },
+  {
+    "revision": "d8f7c489a8eb0bc49018ad8f12526422",
+    "url": "/labiejroo/interbuild.git/static/media/8.d8f7c489.jpg"
+  },
+  {
+    "revision": "a984544456e79dcb6a4172fe2d882893",
+    "url": "/labiejroo/interbuild.git/static/media/9.a9845444.jpg"
+  },
+  {
+    "revision": "19c89b24b22219522570f81af2153015",
+    "url": "/labiejroo/interbuild.git/static/media/A.19c89b24.jpg"
+  },
+  {
+    "revision": "93899dd0c8175cd347d772ba4ef4ab9b",
+    "url": "/labiejroo/interbuild.git/static/media/B.93899dd0.jpg"
+  },
+  {
+    "revision": "8ae535edf8524437d876",
+    "url": "/labiejroo/interbuild.git/static/css/1.b22a50b1.chunk.css"
+  },
+  {
+    "revision": "d0b8e261a833b2f72a37aaf005e1f702",
+    "url": "/labiejroo/interbuild.git/index.html"
+  }
+];
